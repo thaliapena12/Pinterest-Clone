5c3f4c067ae57f7d56e093acbb20585811ebebe9
Thalia <thaliapena12@gmail.com>
2020-01-27 12:47:51 -0800

    adding pins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_27_185805) do
+ActiveRecord::Schema.define(version: 2020_01_27_202930) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "description", null: false
+    t.string "link", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_pins_on_title", unique: true
+    t.index ["user_id"], name: "index_pins_on_user_id", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
