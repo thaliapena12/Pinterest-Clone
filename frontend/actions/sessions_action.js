27339ff49d5c9682d85288c9af797528600d7f2d
Thalia <thaliapena12@gmail.com>
2020-01-28 15:07:05 -0800

    add frontend folder

---
+++ frontend/actions/sessions_action.js
@@ -0,0 +1,12 @@
+export const SIGNIN = "SIGNIN";
+export const SIGNOUT = "LOGOUT";
+export const SIGNUP = "SIGNUP";
+
+export const FETCH_USER = "FETCH_USER";
+export const RECEIVE_USER = "RECEIVE_USER";
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+// export const FOLLOW_USER = "FOLLOW_USER";
+// export const UNFOLLOW_USER = "UNFOLLOW_USER";
